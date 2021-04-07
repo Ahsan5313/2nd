48805a65e2781ea13ab7637836c95b3023de6b86
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-07 13:42:11 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var Num1=parseFloat(prompt("Enter 1st number : "));
+var Num2=parseFloat(prompt("Entere 2nd numbe :  "));
+var Num3=parseFloat(prompt("Enter 3rd number : "));
+var result=Num1>Num2 && Num1>Num3?"Num1 is large ":Num2>Num3 && Num2>Num1?"Num2 is large":"Num3 is large";
+document.write(result);
+
+
